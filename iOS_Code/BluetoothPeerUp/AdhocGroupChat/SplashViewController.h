6eb47959f5bab665beb0f6888825665f24bf5cd9
Gaganpreet Singh <gaganpsingh@krishnais.com>
2015-12-08 17:30:24 +0530

    Copyrights removed

--- iOS_Code/BluetoothPeerUp/AdhocGroupChat/SplashViewController.h
+++ iOS_Code/BluetoothPeerUp/AdhocGroupChat/SplashViewController.h
@@ -1,10 +1,4 @@
-//
-//  SplashViewController.h
-//  Karaoke
-//
-//  Created by Krishna_Mac_3 on 20/03/14.
-//  Copyright (c) 2014 Krishna_Mac_3. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 
